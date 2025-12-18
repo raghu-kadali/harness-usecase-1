68e1adc10499fb745b213eaf380cd77966124a95
raghu-kadali <raghu2.kadali@gmail.com>
2025-12-18 12:54:08 +0530

    k

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "project_id" {
+  description = "kubernetes-477004"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+  default     = "us-central1-a"
+}
